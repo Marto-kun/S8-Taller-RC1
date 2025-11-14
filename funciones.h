476631ea280d8942716db00efd34693c0766f223
Marto-kun <martinvozme456@gmail.com>
2025-11-13 21:41:49 -0500

    Corrección de la función BuscarProd: se cambió el tipo de retorno a int y se mejoró la lógica de búsqueda.

--- funciones.h
+++ funciones.h
@@ -3,21 +3,32 @@
 #include <stdbool.h>
 
 //Funcion para buscar producto por nombre
-bool BuscarProd(char entrada[], char productos[][25])
+int BuscarProd(char entrada[], char *productos[][25])
 {
     bool encontrado = false;
     for (int i = 0; i < 5; i++)
     {
 
-        if (strcmp(entrada, productos[i]) == 0)
+        if (strcmp(entrada, *productos[i]) == 0)
         {
             encontrado = true;
-            return encontrado;
+            return i;
             break;
         }
     }
     if (encontrado == false)
     {
-        return encontrado;
+        return -1;
+    }
+}
+
+
+//Funcion para eliminar productos
+void EliminarProd(char entrada[],char *productos[][25], float *fabricacion[][2]){
+    int encontrado = BuscarProd(entrada, productos); // Se busca el producto a eliminar
+    if(encontrado != -1){
+        *productos[encontrado] = "\0";
+        fabricacion[encontrado][0] = 0;
+        fabricacion[encontrado][1] = 0;
     }
 }
